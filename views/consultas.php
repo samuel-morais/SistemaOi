8390f7f54188bc315bafab470907ab254eac46c8
samuel-morais <samuel.moraisbsb@gmail.com>
2019-05-29 21:49:09 -0300

    Criando um formulário para consultas de circuitos

--- views/consultas.php
+++ views/consultas.php
@@ -7,10 +7,30 @@ $dbname = "base_oi";
 
 $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
 
-$consultas = $_POST['Circuito'];
-
-echo " Circuitos: $consultas";
 
 ?>
 
+<!DOCTYPE html>
+<html lang="pt-br">
 	
+	 <html>
+        <head>
+     		<title> CONSULTAS </title>
+    	</head>
+	<body>
+    	<hr>
+    <center><font face="Arial" size="5" color="#008000"><b>Consultas</b></font>
+    	<hr>
+    <font face="Arial" size="2"><b>Informe o Circuito para consulta:</b></font>
+    <br><br>
+
+    	<tr>
+    <form method="POST" action="consultas.php">
+    <td><font face="Arial" size="2"><b></b></font></td><td><input type="text" name="consul_web" maxlength="12" size="13">
+    <input type="submit" value=" Ok "></td>
+    </form>
+    <br>
+    	<center><input type=button value=" Voltar " OnClick="history.back()"></center>
+    <hr>
+    </body>
+    </html>
